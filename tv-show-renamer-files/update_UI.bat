46d7313b61e1780cf69574a8e933e81b6a4c0380
Thomas Alex <thomas.a@tiflabs.in>
2021-05-15 20:46:19 +0530

    - Restructured files - Renamed various versions to make it clear - Upgraded python env to 3.9 - Updated UI

--- tv-show-renamer-files/update_UI.bat
+++
@@ -1,2 +0,0 @@
-"D:\Documents\TIF Labs\PyQt\gui-venv\Scripts\pyuic5.exe" -x main_ui.ui -o main_ui.py
-"D:\Documents\TIF Labs\PyQt\gui-venv\Scripts\pyuic5.exe" -x show_select_dlg.ui -o show_select_dlg.py
